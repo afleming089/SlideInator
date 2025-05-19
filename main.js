bc4434783eac3f8370bf3b95fffd26f37cc793b3
Anton Fleming <aflemingrocks089@gmail.com>
2025-05-19 16:13:44 -0500

    Got react and vite running together

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 const { app, BrowserWindow } = require("electron");
+require("@electron/remote/main").initialize();
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 400,
     height: 400,
     resizable: true,
+    webPreferences: {
+      enableRemoteModule: true,
+    },
   });
 
-  win.loadFile("./render/dashboard/dashboard.html");
+  win.loadURL("http://localhost:3000/");
 };
 
 app.whenReady().then(() => {
   createWindow();
+});
+
+// macOS
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
